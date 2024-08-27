a48f7fbb0ffb7a774f6b6b413fd705d1113bfe12
Matt Welson <matt@digitaltransformation.co.nz>
2024-08-27 16:16:00 +1200

    init

---
+++ src/app/convex-provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
+import { ConvexReactClient } from "convex/react";
+import type { PropsWithChildren } from "react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export function ConvexClientProvider({ children }: PropsWithChildren) {
+  return (
+    <ConvexAuthNextjsProvider client={convex}>
+      {children}
+    </ConvexAuthNextjsProvider>
+  );
+}
